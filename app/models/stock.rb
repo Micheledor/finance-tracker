ee96fdd0d4ebce3315c97676f2582d82c181599b
Michele Dorazio <michele.dorazio96@gmail.com>
2021-05-24 17:40:30 +0200

    handling errors

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,10 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
-    new(ticker: ticker, name: client.company(ticker).company_name ,last_price: client.price(ticker))
+    begin
+                                  new(ticker: ticker, name: client.company(ticker).company_name ,last_price: client.price(ticker))
+    rescue => exception
+      return nil
+    end  
   end  
 end
