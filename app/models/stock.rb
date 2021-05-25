e0a1259d1e1441318256d840f70c9ff257f7b633
Michele Dorazio <michele.dorazio96@gmail.com>
2021-05-25 12:48:28 +0200

    track stock from UI

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -8,9 +8,13 @@ validates :name, :ticker, presence: true
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
     begin
-                                  new(ticker: ticker, name: client.company(ticker).company_name ,last_price: client.price(ticker))
+                                  new(ticker: ticker.upcase, name: client.company(ticker).company_name ,last_price: client.price(ticker))
     rescue => exception
       return nil
     end  
+  end
+
+  def self.check_db(sym)
+    where(ticker: sym).first
   end  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   has_many :stocks, through: :user_stocks
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  def stock_already_tracked?(sym)
+    stocks.where(ticker: sym).exists?
+    #stock = Stock.check_db(sym)
+    #return false unless stock
+  end       
+
+  def under_stock_limit?
+    stocks.count < 10
+  end
+
+  def can_track_stock?(sym)
+    under_stock_limit? && !stock_already_tracked?(sym)
+  end  
 end
