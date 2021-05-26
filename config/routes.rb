3d2e6f85d6d0180e6e332f022261fcdffa2b6fb5
Michele Dorazio <michele.dorazio96@gmail.com>
2021-05-26 11:48:13 +0200

    ajax call fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :user_stocks, only: [:create, :destroy]
   resources :friendships, only: [:create, :destroy]
+  get 'search_stock', to: 'stocks#search'
   resources :users, only: [:show]
   get 'friends', to: 'users#friends'
   get 'my_portfolio', to: 'users#my_portfolio'
-  get 'search_stock', to: 'stocks#search'
   get 'search_friend', to: 'users#search'
 end
